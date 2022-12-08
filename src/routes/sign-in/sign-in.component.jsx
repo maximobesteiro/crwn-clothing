101f69728f81a9e5d8aa7241d992c02da74470c5
Máximo Besteiro <maximo.besteiro@mightynetworks.com>
2022-12-08 14:37:44 +0000

    Discarding sign in with redirect implementation (we won't be using it in the project)

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,28 +1,9 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
-
 import {
-  auth,
   signInWithGooglePopup,
-  signInWithGoogleRedirect,
   createUserDocumentFromAuth
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
-  useEffect(() => {
-    async function getRedirectResponse() {
-      const response = await getRedirectResult(auth);
-
-      if (response) {
-        createUserDocumentFromAuth(response.user);
-      }
-    }
-
-    getRedirectResponse()
-      // make sure to catch any error
-      .catch(console.error);
-  }, []);
-
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     createUserDocumentFromAuth(user);
@@ -32,7 +13,6 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
     </div>
   );
 };
