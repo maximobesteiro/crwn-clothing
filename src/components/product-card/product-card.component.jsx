6d9efa852c9775441f7a54a391d8727f06c6a90e
Máximo Besteiro <maximo.besteiro@mightynetworks.com>
2023-01-27 22:14:15 +0100

    Product card component + styling

---
+++ src/components/product-card/product-card.component.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import Button from '../button/button.component';
+
+import './product-card.styles.scss';
+
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
+
+  return (
+    <div className="product-card-container">
+      <img src={imageUrl} alt={name} />
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <Button buttonType="inverted">Add to card</Button>
+    </div>
+  );
+};
+
+ProductCard.propTypes = {
+  product: PropTypes.object
+};
+
+export default ProductCard;
